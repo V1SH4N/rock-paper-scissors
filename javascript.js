14506fa3ea9e7e14a6393f0dbe319f539f366a5f
V1SH4N <vishandaby305@gmail.com>
2024-08-27 12:29:25 +0400

    Create UI for rock-paper-scissor game

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3);
     switch(computerChoice) {
@@ -11,80 +10,91 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let humanChoice = prompt("Input Choice: ");
-    return humanChoice.toLowerCase();
-}
-
-
-
-function playGame(){
-    let computerScore = 0;
-    let humanScore = 0;
-
-    function playRound(humanChoice, computerChoice){
-        switch(computerChoice){
-            case "rock"
-            :{
-                switch(humanChoice){
-                    case "rock":
-                        console.log("It's a tie");
-                        break;
-                    case "paper":
-                        console.log("You win! Paper beats rock");
-                        humanScore += 1;
-                        break;
-                    case "scissor":
-                        console.log("You lose! Rock beats scissor");
-                        computerScore += 1;
-                        break;
-                }
-                break;
+function playRound(humanChoice, computerChoice){
+    switch(computerChoice){
+        case "rock"
+        :{
+            switch(humanChoice){
+                case "rock":
+                    return("It's a tie");
+                case "paper":
+                    humanScore += 1;
+                    return("Paper beats rock");
+                case "scissor":
+                    computerScore += 1;
+                    return("Scissor loses to rock");
             }
-            case "paper"
-            :{
-                switch(humanChoice){
-                    case "rock":
-                        console.log("You lose! Paper beats rock");
-                        computerScore += 1;
-                        break;
-                    case "paper":
-                        console.log("It's a tie");
-                        break;
-                    case "scissor":
-                        console.log("You win! Scissor beats paper");
-                        humanScore += 1;
-                        break;
-                }
-                break;
-            }
-            case "scissor"
-            :{
-                switch(humanChoice){
-                    case "rock":
-                        console.log("You win! Rock beats scissor");
-                        humanScore += 1;
-                        break;
-                    case "paper":
-                        console.log("You lose! Scissor beats paper");
-                        computerScore += 1;
-                        break;
-                    case "scissor":
-                        console.log("It's a tie");
-                        break;
-                }
-                break;
-            }  
         }
+        case "paper"
+        :{
+            switch(humanChoice){
+                case "rock":
+                    computerScore += 1;
+                    return("Rock loses to paper");
+                case "paper":
+                    return("It's a tie");
+                case "scissor":
+                    humanScore += 1;
+                    return("Scissor beats paper");
+            }
+        }
+        case "scissor"
+        :{
+            switch(humanChoice){
+                case "rock":
+                    humanScore += 1;
+                    return("Rock beats scissor");
+                case "paper":
+                    computerScore += 1;
+                    return("Paper loses to scissor");
+                case "scissor":
+                    return("It's a tie");
+            }
+        }  
     }
-
-    for (let i = 0; i < 5; i++){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-    (computerScore < humanScore) ? console.log("You are the overall winner") : console.log("You are the overall loser")
 }
 
+let computerScore = 0;
+let humanScore = 0;
 
-playGame();
+const buttons = document.querySelector('#buttons');
+const gameStatus = document.querySelector('#status');
+const scoreHuman = document.querySelector('#scoreHuman')
+const scoreComputer = document.querySelector('#scoreComputer');
+///const output = document.querySelector('#output');
+
+buttons.addEventListener('click',(Event) =>{
+
+    let resultStatus;
+
+    if (humanScore < 5 && computerScore < 5){
+        switch (Event.target.id){
+            case 'rock':
+                resultStatus = playRound('rock',getComputerChoice());
+                break;
+            case 'paper':
+                resultStatus = playRound('paper',getComputerChoice());
+                break;
+            case 'scissor':
+                resultStatus = playRound('scissor',getComputerChoice());
+                break
+        } 
+    }
+
+    scoreHuman.textContent = humanScore;
+    scoreComputer.textContent = computerScore;
+
+    if (humanScore == 5){
+        gameStatus.textContent = "Congratulation, you won!!";
+        return;
+    } else if (computerScore == 5){
+        gameStatus.textContent = "You lost.";
+        return;
+    }
+    gameStatus.textContent = resultStatus;
+})
+
+
+
+
+
